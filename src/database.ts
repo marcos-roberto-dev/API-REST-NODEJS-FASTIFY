0515e2d7cb23e8cb63d28902fcc31776f6051522
Marcos Roberto <marcosprojetosdev@gmail.com>
2025-03-24 07:42:16 -0300

    add dotenv support and update database configuration to use environment variable

--- src/database.ts
+++ src/database.ts
@@ -1,9 +1,11 @@
+import 'dotenv/config'
+
 import { knex as setupKnex, Knex } from 'knex'
 
 export const configKnex: Knex.Config = {
   client: 'sqlite',
   connection: {
-    filename: './database/app.db',
+    filename: process.env.DATABASE_URL!,
   },
   useNullAsDefault: true,
   migrations: {
