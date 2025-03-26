47f469a346c7b165aba66dcf098b12edfe842ae4
Marcos Roberto <marcosprojetosdev@gmail.com>
2025-03-26 08:43:31 -0300

    add session ID validation middleware and apply it to transactions API routes

--- src/routes/transactions.routes.ts
+++ src/routes/transactions.routes.ts
@@ -2,34 +2,48 @@ import { FastifyInstance } from 'fastify'
 import { knex } from '../database'
 import { transactionSchema } from '../schemas/transactions.schema'
 import { z as zod } from 'zod'
+import { checkSessionIdExists } from '../middlewares/check-session-id-exists'
 
 export async function transactionsRoutes(app: FastifyInstance) {
-  app.get('/', async () => {
-    const transactions = await knex('transactions').select('*')
+  app.get('/', { preHandler: [checkSessionIdExists] }, async (request) => {
+    const sessionId = request.cookies.sessionId
+
+    const transactions = await knex('transactions')
+      .where({ session_id: sessionId })
+      .select('*')
     return { transactions }
   })
 
-  app.get('/:id', async (request) => {
+  app.get('/:id', { preHandler: [checkSessionIdExists] }, async (request) => {
+    const sessionId = request.cookies.sessionId
     const getTransactionParamsSchema = zod.object({
       id: zod.string().uuid(),
     })
     const { id } = getTransactionParamsSchema.parse(request.params)
-    const transaction = await knex('transactions').where({ id }).first()
+    const transaction = await knex('transactions')
+      .where({ id, session_id: sessionId })
+      .first()
 
     return {
       transaction,
     }
   })
 
-  app.get('/summary', async () => {
-    const summary = await knex('transactions')
-      .sum('amount', { as: 'amount' })
-      .first()
+  app.get(
+    '/summary',
+    { preHandler: [checkSessionIdExists] },
+    async (request) => {
+      const sessionId = request.cookies.sessionId
+      const summary = await knex('transactions')
+        .sum('amount', { as: 'amount' })
+        .where({ session_id: sessionId })
+        .first()
 
-    return {
-      summary,
-    }
-  })
+      return {
+        summary,
+      }
+    },
+  )
 
   app.post('/', async (request, reply) => {
     const createTransactionSchema = transactionSchema
