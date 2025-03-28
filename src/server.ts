b864a440229835cdb91f1965ae633961b5a4398e
Marcos Roberto <marcosprojetosdev@gmail.com>
2025-03-28 16:28:56 -0300

    refactor: separate app initialization into app.ts and update server.ts to use it; add testing setup for transactions routes

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,4 @@
-import fastify from 'fastify'
-import { transactionsRoutes } from './routes/transactions.routes'
-import cookie from '@fastify/cookie'
-
-const app = fastify()
-
-app.register(cookie)
-
-app.register(transactionsRoutes, {
-  prefix: '/transactions',
-})
+import { app } from './app'
 
 app.listen({ port: 3333 }).then(() => {
   console.log('Server is running on http://localhost:3333')
